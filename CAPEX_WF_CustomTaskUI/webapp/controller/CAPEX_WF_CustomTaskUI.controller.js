dfe55590cd213d4830b5f0ce14c120b008806bb4
neoray12 <neoray12@hotmail.com>
2021-01-26 10:10:43 +0000

    adding Northwind destination

--- CAPEX_WF_CustomTaskUI/webapp/controller/CAPEX_WF_CustomTaskUI.controller.js
+++ CAPEX_WF_CustomTaskUI/webapp/controller/CAPEX_WF_CustomTaskUI.controller.js
@@ -10,6 +10,15 @@ sap.ui.define([
 		return Controller.extend("sap.wf.demo.CAPEX_WF_CustomTaskUI.controller.CAPEX_WF_CustomTaskUI", {
 			onInit: function () {
 
+                $.ajax({
+				url: "/sapwfdemoCAPEX_WF_CustomTaskUI/Northwind/V3/Northwind/Northwind.svc/Orders?$top=5&$format=json",
+				method: "GET",
+				async: false,
+				success: function (result, xhr, data) {
+					console.log(data);
+				}
+            }); 
+
 			}
 		});
 	});
